c35d462986bb8761d2dac972eb2e9631e391e58a
Max Ehrlich <maxehr@umiacs.umd.edu>
2018-04-02 12:25:18 -0400

    Fill in code

---
+++ data_sampling/smoothness_tuple_generator.py
@@ -0,0 +1,206 @@
+import sys
+
+sys.path.append('../')
+import data_sampling.motion_aware as motion_aware
+import utils
+import os
+import cv2
+import constants as const
+import file_constants as file_const
+import numpy as np
+import imageio
+import multiprocessing as mp
+import traceback
+
+
+def create_stack_diff(vid, stack_diff_frame_idz):
+    unique_els = np.unique(stack_diff_frame_idz)
+    if (unique_els.shape != stack_diff_frame_idz.shape):
+        print("Something is wrong")
+
+    n_frames = 12
+    total_frames = vid._meta['nframes']
+
+    frames_idx = np.random.choice(range(total_frames), n_frames, replace=False)
+    frames_idx.sort()
+
+    frames = []
+    for i in range(len(frames_idx)):
+        f = vid.get_data(frames_idx[i])
+        rf = cv2.resize(f, (const.nominal_width, const.nominal_height))
+        frames.append(rf)
+
+    stacked_diff_0 = np.stack(frames[:,len(frames)/2], 3)
+    stacked_diff_1 = np.stack(frames[len(frames)/2:], 3)
+
+    stacked_diff = [stacked_diff_0, stacked_diff_1]
+
+    return stacked_diff
+
+
+def sample_video_2(dataset_path, subset_files):
+    sample = np.random.choice(subset_files);
+    slash_index = sample.index('/');
+    activity = sample[:slash_index]
+    file_name = os.path.join(dataset_path, sample)
+    # print(activity,file_name )
+    return activity, file_name
+
+
+def sample_video(dataset_path):
+    dirs = utils.get_dirs(dataset_path);
+    print(len(dirs))
+    sample_activity = np.random.choice(len(dirs))
+    d = dirs[sample_activity];
+    current_dir = os.path.join(dataset_path, d);
+    files = utils.get_files(current_dir, '.avi', append_base=True);
+    sample_vdx = np.random.choice(len(files))
+    print(len(files))
+    current_vdx = files[sample_vdx]
+    return d, current_vdx;
+
+
+def save_tuple(vid, center_frame_idx, stack_diff_frame_idz, save_dir, tuple_idx, lbls_ary, activity_lbl, queue=None):
+    try:
+        #center_img = vid.get_data(center_frame_idx)
+        #if (center_img.shape[0] != const.nominal_height or center_img.shape[1] != const.nominal_width):
+        #   center_img = cv2.resize(center_img, (const.nominal_height, const.nominal_width))
+
+        #imageio.imwrite(save_dir + '/frame' + "%07d" % (tuple_idx) + '.jpg', center_img);
+        stack_diff = create_stack_diff(vid, stack_diff_frame_idz)
+        utils.pkl_write(save_dir + '/frame' + "%07d" % (tuple_idx) + '.pkl', stack_diff)
+
+        visualize = False
+        if (visualize):
+            for i in range(stack_diff.shape[3]):
+                cv2.imwrite(save_dir + '/frame_' + '%07d' % (tuple_idx) + '_' + str(i) + '_pkl.jpg',
+                            cv2.cvtColor(stack_diff[:, :, :, i], cv2.COLOR_RGB2BGR))
+                # plt.imshow(stack_diff[:, :, :,i]);
+                # plt.axis('off')
+                # plt.savefig(save_dir + '/frame_' + '%07d' % (tuple_idx) +'_'+ str(i)+'_pkl.jpg')
+        queue.put(True)
+    except:
+        pass
+        queue.put(False)
+        queue.put(tuple_idx)
+
+
+if __name__ == '__main__':
+    dataset_path = utils.get_dataset_path(file_const.dataset_name)
+    pkls_path = dataset_path + '_pkls'
+    save_dir = dataset_path + '_tuples_class'
+
+    files_list = None
+
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--is_training', action='store_true')
+
+    args = parser.parse_args()
+
+    ## Take one arg to indicate train or val. Train 0, Val 1
+    if (args.is_training):
+        current_subset = 0  ## Train
+    else:
+        current_subset = 1  ## Val
+
+    if (current_subset == const.Subset.TRAIN.value):
+        files_list = utils.txt_read(os.path.join(dataset_path + '_lists', 'trainlist.txt'))
+        print('**** Train has ', len(files_list))
+        save_dir = os.path.join(save_dir, 'train')
+        max_num_tuplex = 2000  ##200000
+    elif (current_subset == const.Subset.VAL.value):
+        files_list = utils.txt_read(os.path.join(dataset_path + '_lists', 'vallist.txt'))
+        print('**** Val has ', len(files_list))
+        save_dir = os.path.join(save_dir, 'val')
+        max_num_tuplex = 100  ## 20000
+    elif (current_subset == const.Subset.TEST.value):
+        files_list = utils.txt_read(os.path.join(dataset_path + '_lists', 'testlist.txt'))
+        print('*** Test has ', len(files_list))
+        save_dir = os.path.join(save_dir, 'test')
+
+    utils.touch_dir(save_dir)
+    tuple_idx = 0
+    tuple_idx = max(0, utils.last_tuple_idx(save_dir) - 5)  ## As caution, regenerate last 5 tuples
+    activity_list = sorted(utils.get_dirs(dataset_path));
+    print('activity_list ', len(activity_list), activity_list)
+
+    lbls_file = os.path.join(save_dir, 'lbl.pkl')
+
+    if (tuple_idx == 0):
+        lbls_ary = np.ones(max_num_tuplex, dtype=np.int32) * -1;  ## Invalid Activity
+    else:
+        lbls_ary = utils.pkl_read(lbls_file)
+        tuple_idx = 0;
+
+    import time
+
+    start_time = time.time()
+
+    queue_size = 25;
+    queues = [None] * queue_size
+    processes = [None] * queue_size
+    idx = 0
+    while tuple_idx < max_num_tuplex:
+        if (lbls_ary[tuple_idx] != -1):
+            tuple_idx += 1
+            continue;
+        # activity, current_vdx = sample_video(dataset_path)
+        activity, current_vdx = sample_video_2(dataset_path, files_list)
+        activity_lbl = activity_list.index(activity)
+
+        # print(activity_lbl )
+        vdx_path = os.path.join(current_vdx)
+        name, ext = utils.get_file_name_ext(vdx_path);
+        try:
+            vid = imageio.get_reader(vdx_path, 'ffmpeg')
+        except:
+            traceback.print_exc()
+            continue
+
+        pkl_file = os.path.join(pkls_path, activity, name) + '.pkl'
+        flow_mag = utils.pkl_read(pkl_file)
+
+        center_frame_idx, stack_diff_frame_idz = motion_aware.sample_high_motion_frames(vid, flow_mag, vdx_path)
+        if (center_frame_idx is not None):
+
+            # save_tuple(vid, center_frame_idx, stack_diff_frame_idz, save_dir, tuple_idx, lbls_ary, activity_lbl)
+            # tuple_idx += 1
+            queues[idx] = mp.Queue()
+            processes[idx] = mp.Process(target=save_tuple, args=(
+            vid, center_frame_idx, stack_diff_frame_idz, save_dir, tuple_idx, lbls_ary, activity_lbl, queues[idx]))
+            processes[idx].start()
+            lbls_ary[tuple_idx] = activity_lbl;
+            idx += 1;
+            tuple_idx += 1
+
+            if (idx == queue_size):
+                for i in range(queue_size):
+                    processes[i].join()
+                    success = queues[i].get()
+                    if (not success):
+                        failure_tuple_idx = queues[i].get()
+                        lbls_ary[failure_tuple_idx] = -1
+                        print('Something went wrong')
+                idx = 0;
+                utils.pkl_write(lbls_file, lbls_ary);
+
+            if (tuple_idx % 100 == 0):
+                print('Number of tuples ', tuple_idx, '/ ', max_num_tuplex)
+                print(activity, current_vdx)
+
+    for i in range(idx):
+        processes[i].join()
+        success = queues[i].get()
+        if (not success):
+            print('Something went wrong')
+            failure_tuple_idx = queues[i].get()
+            lbls_ary[failure_tuple_idx] = -1
+
+    utils.pkl_write(lbls_file, lbls_ary);
+    for i in range(10):
+        print(activity_list[lbls_ary[i]], end="\t")
+
+    elapsed_time = time.time() - start_time
+    print('elapsed_time :', elapsed_time)
---
+++ data_sampling/smoothness_tuple_loader.py
@@ -0,0 +1,297 @@
+import sys
+
+sys.path.append('../')
+import os
+import file_constants as file_const
+import data_sampling.data_args as data_args
+import constants as const
+import numpy as np
+import cv2
+import utils
+import traceback
+import imageio
+
+import matplotlib
+
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+
+class TupleLoader:
+    def __init__(self, args):
+        self._tuples_path = utils.dataset_tuples(utils.get_dataset_path(file_const.dataset_name))
+
+        train_tuple_path = os.path.join(self._tuples_path, 'train')
+        val_tuple_path = os.path.join(self._tuples_path, 'val')
+        self.train_lbls_ary = utils.pkl_read(os.path.join(train_tuple_path, 'lbl.pkl'))
+        self._num_training = self.train_lbls_ary.shape[0]
+        self.val_lbls_ary = utils.pkl_read(os.path.join(val_tuple_path, 'lbl.pkl'))
+        self._num_val = self.val_lbls_ary.shape[0]
+        print('Num Training ', self._num_training, ' Num Validation', self._num_val)
+
+        self._gen_nearby_frame = args[data_args.gen_nearby_frame]
+        self._data_augmentation_enabled = args[data_args.data_augmentation_enabled]
+        print('Near By ', self._gen_nearby_frame)
+
+    # def img_at_index(self,index,subset,batch_idx=None,lbl=None,verbose=False):
+    #     im = None;
+    #
+    #     im = cv2.imread(self._tuples_path + '/'+subset+'/frame' + '%07d' % (index) + '.jpg')
+    #     rand_crop = np.random.rand();
+    #     rand_rgb_channel = np.random.choice(3);
+    #     y = int(rand_crop *(im.shape[0] - const.frame_height))
+    #     x = int(rand_crop * (im.shape[1] - const.frame_width))
+    #     im = im[y:y+const.frame_height,x:x+const.frame_width,rand_rgb_channel];
+    #     im = im[:,:,np.newaxis]
+    #     im = np.repeat(im,3,axis=2);
+    #
+    #     if(verbose):
+    #         prefix = 'tuple_'
+    #         suffix = '_img';
+    #         cv2.imwrite(file_const.dump_path + prefix + str(batch_idx) + '_' + str(lbl) + suffix + '.png',im)
+    #     return im;
+
+    def pkl_at_index(self, index, subset, ordered=True, batch_idx=None, lbl=None, verbose=False):
+
+        pkl_all = utils.pkl_read(self._tuples_path + '/' + subset + '/frame' + '%07d' % (index) + '.pkl')
+
+        stacked_diff_all = []
+        for i in len(pkl_all):
+            pkl = pkl_all[i]
+            rand_crop = np.random.rand()
+            y = int(rand_crop * (pkl.shape[0] - const.frame_height))
+            x = int(rand_crop * (pkl.shape[1] - const.frame_width))
+
+            rand_rgb_channel = np.random.choice(3);
+            stack_diff = np.zeros((const.frame_height, const.frame_width, const.context_channels))
+            if ordered or i == 1:
+                frames_order = np.arange(const.context_channels + 1)
+            else:
+                frames_order = np.random.permutation(const.context_channels + 1)
+
+            for j in range(const.context_channels):
+                current_frame = pkl[y:y + const.frame_height, x:x + const.frame_width, rand_rgb_channel,
+                                frames_order[j]];
+                next_frame = pkl[y:y + const.frame_height, x:x + const.frame_width, rand_rgb_channel,
+                             frames_order[j + 1]];
+                stack_diff[:, :, j] = current_frame.astype(np.int32) - next_frame.astype(np.int32);
+
+            stacked_diff_all.append(stack_diff)
+
+        # if(verbose):
+        #     prefix = 'tuple_'
+        #     suffix = '_pkl'
+        #     images = []
+        #     for j in range(5):
+        #         im = stack_diff[:, :, j];
+        #         im = ((im - np.amin(im)) / np.amax(im) - np.amin(im)) * 255
+        #         im = im.astype(np.uint8)
+        #         images.append(im)
+        #         cv2.imwrite(file_const.dump_path + prefix + str(batch_idx) + '_' + str(ordered) + '_' + str(j) + suffix + '.png', im)
+        #     for j in range(5):
+        #         images.append(np.zeros((const.frame_height, const.frame_width), dtype=np.uint8))
+        #     imageio.mimsave(file_const.dump_path + prefix + str(batch_idx) + '_' + str(ordered) + '_' + str(lbl) + suffix + '.gif', images,
+        #                     duration=0.5)
+        #     if(ordered == False):
+        #         self.pkl_at_index(index,subset,ordered=True,batch_idx=batch_idx,lbl=lbl,verbose=True);
+
+        return stacked_diff_all
+
+    def unsupervised_next(self, subset, fix_label=None):
+        subset_name = ''
+        subset_size = 0
+        if (subset == const.Subset.TRAIN):
+            subset_name = 'train'
+            subset_size = self._num_training
+            class_lbls = self.train_lbls_ary
+            ratio = 3
+        elif (subset == const.Subset.VAL):
+            subset_name = 'val'
+            subset_size = self._num_val
+            class_lbls = self.val_lbls_ary
+            ratio = 2;
+        elif (subset == const.Subset.TEST):
+            img_set = self._test_activities
+
+        pos_tuple = np.random.randint(low=0, high=subset_size, size=(const.batch_size));
+        neg_tuple = np.random.randint(low=0, high=subset_size, size=(const.batch_size));
+
+        #words = np.zeros((const.batch_size, const.frame_height, const.frame_width, const.frame_channels))
+        contexts = np.zeros((const.batch_size, 2, const.frame_height, const.frame_width, const.context_channels))
+        if self._gen_nearby_frame:
+            nearby = np.zeros((const.batch_size, const.frame_height, const.frame_width, const.frame_channels))
+
+        labels = np.zeros((const.batch_size), dtype=np.int32)
+        postive_ratio = 1 / ratio;
+        if (fix_label == None):
+            sampling_ratio = np.random.rand(const.batch_size);
+        elif (fix_label == 1):
+            sampling_ratio = np.zeros(const.batch_size);
+        elif (fix_label == -1):
+            sampling_ratio = np.random.rand(const.batch_size) * (1 - postive_ratio) + postive_ratio;
+        neg_count = 0
+        pos_count = 0
+        order_neg_count = 0
+        verbose_in_dump = False;
+        for batch_idx in np.arange(0, const.batch_size):
+
+            if (sampling_ratio[batch_idx] < postive_ratio):
+
+                pos_count += 1
+                labels[batch_idx] = 1;
+                if (class_lbls[pos_tuple[batch_idx]] != -1):
+                    # words[batch_idx, :, :] = self.img_at_index(pos_tuple[batch_idx], subset_name, batch_idx=batch_idx,
+                    #                                            lbl=1, verbose=verbose_in_dump)
+                    c_0, c_1 = self.pkl_at_index(pos_tuple[batch_idx], subset_name,
+                                      batch_idx=batch_idx, lbl=1, verbose=verbose_in_dump)
+                    contexts[batch_idx, 0, :, :] = c_0
+                    contexts[batch_idx, 1, :, :] = c_1
+                else:
+                    print("Something wrong with tuple", pos_tuple[batch_idx], file=sys.stderr)
+                    #words[batch_idx, :, :] = self.img_at_index(0, subset_name)
+                    c_0, c_1 = self.pkl_at_index(0, subset_name)
+                    contexts[batch_idx, 0, :, :] = c_0
+                    contexts[batch_idx, 1, :, :] = c_1
+
+            else:
+
+                labels[batch_idx] = -1;
+                if (class_lbls[pos_tuple[batch_idx]] != -1):
+                    # words[batch_idx, :, :] = self.img_at_index(pos_tuple[batch_idx], subset_name, batch_idx=batch_idx,
+                    #                                            lbl=0, verbose=verbose_in_dump)
+                    if ((sampling_ratio[batch_idx] - postive_ratio) / (1 - postive_ratio) <= 1):
+                        c_0, c_1 = self.pkl_at_index(pos_tuple[batch_idx], subset_name, ordered=False,
+                                                                      batch_idx=batch_idx, lbl=0,
+                                                                      verbose=verbose_in_dump)
+                        contexts[batch_idx, 0, :, :] = c_0
+                        contexts[batch_idx, 1, :, :] = c_1
+                        order_neg_count += 1
+                    else:
+                        c_0, c_1 = self.pkl_at_index(neg_tuple[batch_idx], subset_name,
+                                                                      batch_idx=batch_idx, lbl=0,
+                                                                      verbose=verbose_in_dump)
+
+                        contexts[batch_idx, 0, :, :] = c_0
+                        contexts[batch_idx, 1, :, :] = c_1
+                        neg_count += 1
+                else:
+                    print("Something wrong with tuple", pos_tuple[batch_idx], file=sys.stderr)
+                    #words[batch_idx, :, :] = self.img_at_index(0, subset_name)
+                    c_0, c_1 = self.pkl_at_index(0, subset_name, ordered=False)
+                    contexts[batch_idx, 0, :, :] = c_0
+                    contexts[batch_idx, 1, :, :] = c_1
+
+        # print(pos_count,neg_count,order_neg_count)
+
+        labels[labels == -1] = 0
+        labels_hot_vector = np.zeros((const.batch_size, 2))
+        labels_hot_vector[np.arange(const.batch_size), labels] = 1
+
+        if self._gen_nearby_frame:
+            return nearby, contexts, labels_hot_vector
+        else:
+            return contexts, labels_hot_vector
+
+    def supervised_next(self, subset, fix_label=None):
+        raise NotImplementedError()
+
+        subset_name = ''
+        subset_size = 0
+        if (subset == const.Subset.TRAIN):
+            subset_name = 'train'
+            subset_size = self._num_training
+            class_lbls = self.train_lbls_ary
+        elif (subset == const.Subset.VAL):
+            subset_name = 'val'
+            subset_size = self._num_val
+            class_lbls = self.val_lbls_ary
+        elif (subset == const.Subset.TEST):
+            img_set = self._test_activities
+
+        if (fix_label == None):
+            tuple = np.random.randint(low=0, high=subset_size, size=(const.batch_size));
+        else:
+            samples_pool = np.where(class_lbls == fix_label)[0]
+            tuple = np.random.choice(samples_pool, const.batch_size)
+
+        words = np.zeros((const.batch_size, const.frame_height, const.frame_width, const.frame_channels))
+        contexts = np.zeros((const.batch_size, const.frame_height, const.frame_width, const.context_channels))
+        if self._gen_nearby_frame:
+            nearby = np.zeros((const.batch_size, const.frame_height, const.frame_width, const.frame_channels))
+
+        labels = np.zeros((const.batch_size), dtype=np.int32)
+
+        for batch_idx in np.arange(0, const.batch_size):
+            if (class_lbls[tuple[batch_idx]] != -1):
+                labels[batch_idx] = class_lbls[tuple[batch_idx]];
+                words[batch_idx, :, :] = self.img_at_index(tuple[batch_idx], subset_name)
+                contexts[batch_idx, :, :] = self.pkl_at_index(tuple[batch_idx], subset_name)
+            else:
+                print("Something wrong with tuple", tuple[batch_idx], file=sys.stderr)
+                labels[batch_idx] = class_lbls[0];
+                words[batch_idx, :, :] = self.img_at_index(0, subset_name)
+                contexts[batch_idx, :, :] = self.pkl_at_index(0, subset_name)
+
+        labels_hot_vector = np.zeros((const.batch_size, file_const.num_classes))
+        labels_hot_vector[np.arange(const.batch_size), labels] = 1
+        if self._gen_nearby_frame:
+            return words, nearby, contexts, labels_hot_vector
+        else:
+            return words, contexts, labels_hot_vector
+
+    ## subset : indicate whether to use Train or Val
+    ## fix_label : typically using with validation set, to force all tuple belong to certain class.
+    ## For example, you want a mini-batch size with all tuples below to 'horse riding' activity.
+    ## supervised: indicate with the labels are Pos / Neg classification in case of unsupervised or Activity Label in case of supervised
+
+    def next(self, subset, fix_label=None, supervised=False):
+        if (supervised):
+            return self.supervised_next(subset, fix_label)
+        else:
+            return self.unsupervised_next(subset, fix_label)
+
+
+def save_pkls(prefix, context, lbls, suffix):
+    # for filename in filenames:
+    #
+    # imageio.mimsave('/path/to/movie.gif', images)
+
+    for i in range(context.shape[0]):
+        current_cntxt = np.reshape(context[i], (const.frame_height, const.frame_width, 5))
+        images = []
+        for j in range(5):
+            im = current_cntxt[:, :, j];
+            im = ((im - np.amin(im)) / np.amax(im) - np.amin(im)) * 255
+            im = im.astype(np.uint8)
+            images.append(im)
+            cv2.imwrite(file_const.dump_path + prefix + str(i) + '_' + str(j) + suffix + '.png', im)
+        for j in range(5):
+            images.append(np.zeros((const.frame_height, const.frame_width), dtype=np.uint8))
+        imageio.mimsave(file_const.dump_path + prefix + str(i) + '_' + str(j) + suffix + '.gif', images, duration=0.5)
+        # plt.imshow(current_cntxt[:,:,j]);
+        # plt.axis("off")
+        # plt.savefig(file_const.dump_path + prefix + str(i) + '_' + str(j) + suffix+'.png',bbox_inches='tight')
+
+
+def save_imgs(prefix, imgs, lbls, suffix):
+    for i in range(imgs.shape[0]):
+        cv2.imwrite(file_const.dump_path + prefix + str(i) + '_' + str(lbls[i]) + suffix + '.png',
+                    np.reshape(imgs[i], (const.frame_height, const.frame_width, const.frame_channels)))
+
+
+if __name__ == '__main__':
+    args = dict()
+    args[data_args.gen_nearby_frame] = False;
+    args[data_args.data_augmentation_enabled] = False
+
+    vdz_dataset = TupleLoader(args);
+    import time
+
+    start_time = time.time()
+    words, contexts, lbls = vdz_dataset.next(const.Subset.VAL, fix_label=-1, supervised=False)
+    elapsed_time = time.time() - start_time
+    print('elapsed_time :', elapsed_time)
+    ## Some visualization for debugging purpose
+    # save_imgs('tuple_',words,lbls,'_img');
+    # save_pkls('tuple_', contexts, lbls, '_pkl');
+    print('Done')
